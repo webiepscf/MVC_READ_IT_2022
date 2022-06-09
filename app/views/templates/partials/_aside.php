a684c9f389066b365b5126dc80bcaedc8ec851df
Pascal Lacroix <webiepscf@gmail.com>
2022-06-09 13:08:12 +0200

    Intégration de la searchbar

--- app/views/templates/partials/_aside.php
+++ app/views/templates/partials/_aside.php
@@ -1,12 +1,7 @@
 <div class="col-lg-4 sidebar pl-lg-5 ftco-animate">
-    <div class="sidebar-box">
-        <form action="#" class="search-form">
-            <div class="form-group">
-                <span class="icon icon-search"></span>
-                <input type="text" class="form-control" placeholder="Type a keyword and hit enter">
-            </div>
-        </form>
-    </div>
+
+    <?php include '../app/views/templates/partials/_search/php'; ?>
+
     <div class="sidebar-box ftco-animate">
         <div class="categories">
             <h3>Categories</h3>
